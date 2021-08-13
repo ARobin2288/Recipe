c1aa79797d353a048109973314d9ad423f7d5e27
ARobin2288 <79115206+ARobin2288@users.noreply.github.com>
2021-08-12 20:12:42 -0500

    checklist & join now button w/js

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-function joining() {
-  let name = prompt("What is your preferred name?");
-  let email = prompt("What's your email address?");
+function myFunction() {
+  // fetching the checkbox by id
+  var doc = document.getElementById("check");
 
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = "Welcome " + name + ", thanks for joining!";
+  // changing the state of checkbox to checked
+  doc.checked = true;
+}
+
+function updateHeading(newHeading) {
+  let heading = document.querySelector("h2");
+  heading.innerHTML = newHeading;
+}
+
+function joining() {
+  let name = prompt("What's your name?");
+  let email = prompt("Your email address?");
+  let age = prompt("How old are you?");
+
+  if (age >= 21) {
+    updateHeading(
+      `Welcome ${name}, don't forget to checkout our Signiture Cocktails section!`);
+  } else {
+    updateHeading(`Welcome ${name}, thanks for joining!`);
+  }
 }
 
 let signupButton = document.querySelector(".joinNow");
