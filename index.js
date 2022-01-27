bfa147eab6b2545cbbe32ba7d8f659b148e634aa
Robin <RobinAndrews22@gmail.com>
2022-01-27 10:26:00 -0600

    added some new js

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-function myFunction() {
-  // fetching the checkbox by id
-  var doc = document.getElementById("check");
-
-  // changing the state of checkbox to checked
-  doc.checked = true;
+//base collection from subscribers, what will be asked for//
+function subscriberInfo(firstName, lastName, age, number, email) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.number = number;
+  this.email = email;
+  this.sayName = function () {
+    console.log("Name: " + this.firstName + " " + this.lastName);
+  };
 }
 
-function updateHeading(newHeading) {
-  let heading = document.querySelector("h2");
-  heading.innerHTML = newHeading;
+//new subscriber var info entered in//
+var robinAndrews = new subscriberInfo(
+  "Robin",
+  "Andrews",
+  33,
+  "630-913-6295",
+  "RobinAndrews22@gmail.com"
+);
+console.log(robinAndrews);
+
+//types of donuts function base//
+function Donut(type, glazed, sweetness, hasSprinkles) {
+  this.type = type;
+  this.glazed = glazed;
+  this.sweetness = sweetness;
+  this.hasSprinkles = hasSprinkles;
+  this.sayType = function () {
+    console.log("type: " + this.type);
+  };
+  this.showSweetness = function () {
+    console.log("Sweetness: " + this.sweetness + "/10");
+  };
+}
+//types of donuts//
+var coconutDonut = new Donut("coconut", false, 8, false);
+console.log(coconutDonut);
+
+var vanillaDonut = new Donut("vanilla", true, 10, true);
+console.log(vanillaDonut);
+
+coconutDonut.tasteGood = false;
+
+vanillaDonut.tasteGood = true;
+
+//format subscriber full name//
+function formatNames(arg1, arg2) {
+  return arg1 + " " + arg2;
 }
 
-function joining() {
-  let name = prompt("What's your name?");
-  let email = prompt("Your email address?");
-  let age = prompt("How old are you?");
+//function of whats being collected//
+function dataFormatter(fullName, email) {
+  const formattedData = {
+    name: fullName,
+    email: email,
+  };
+  return formattedData;
+}
 
-  if (age >= 21) {
-    updateHeading(
-      `Welcome ${name}, don't forget to checkout our cocktail recipes!`);
-  } else {
-    updateHeading(`Welcome ${name}, thanks for joining!`);
+//giving const to the full name format and combining with email//
+function joining(firstName, lastName, email) {
+  const formattedNames = formatNames(firstName, lastName);
+  const formattedData = dataFormatter(formattedNames, email);
+  return formattedData;
+}
+console.log(joining("Robin", "Andrews", "RobinAndrews22@gmail.com"));
+
+//figuring out hourly pay to yearly salary//
+function salary(num1) {
+  function hourPay(x) {
+    return x * 40;
   }
+
+  return hourPay(num1) * 52;
 }
-
-let signupButton = document.querySelector(".joinNow");
-
-signupButton.addEventListener("click", joining);
+console.log(salary(20));
